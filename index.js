e452a127250e5aee86c64c3a64424ab7f71e2f56
Stephen Fudge <sfudge@gmail.com>
2022-06-10 20:28:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// creating variables for i will need to call later on
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// where the info is for each role
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
